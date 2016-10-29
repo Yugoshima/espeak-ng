864d17c7b296067e55a07bfa7902081c4536ca17
Alberto Pettarin <alberto@albertopettarin.it>
2016-10-29 22:23:07 +0200

    Added language selection to build_js. Updated docs accordingly.

--- build_js.sh
+++ build_js.sh
@@ -3,15 +3,27 @@
 # For this script to work you must have emscripten configured in your shell.
 # See http://kripken.github.io/emscripten-site/docs/getting_started/downloads.html
 
-CONFIGURE_FLAGS=--prefix=/usr --without-async --without-mbrola --without-sonic
+# Configure for a native build.
+./configure --prefix=/usr --without-async --without-mbrola --without-sonic &&
 
-# First, we configure for a native build
-./configure $(CONFIGURE_FLAGS) &&
-# Do a native build to create data files
-make &&
-# Re-configure for emscripten
-emconfigure ./configure $(CONFIGURE_FLAGS) &&
+# Do a native build to compile data files.
+if [ "$#" -lt 1 ]
+then
+    # No arguments: compile data for all languages.
+    make
+else
+    # One or more arguments: compile data for the specified languages.
+    for arg in "$@"
+    do
+        make "$arg"
+    done
+fi
+
+# Re-configure for emscripten.
+emconfigure ./configure --prefix=/usr --without-async --without-mbrola --without-sonic &&
+
 # Build espeak-ng library as LLVM IR code.
 emmake make src/libespeak-ng.la &&
+
 # Create Javascript espeak-ng worker.
 emmake make -C emscripten
